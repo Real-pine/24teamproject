bd86f551e8ac961242f6f051a182e0e3a2b6292b
Real-pine <crewger9030@gmail.com>
2024-09-26 18:02:00 +0900

    version error

--- Text_RPG_24Group/Text_RPG_24Group/obj/Debug/net8.0/Text_RPG_24Group.AssemblyInfo.cs
+++ Text_RPG_24Group/Text_RPG_24Group/obj/Debug/net8.0/Text_RPG_24Group.AssemblyInfo.cs
@@ -14,7 +14,6 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb4264eb4c53010f98af1130d9d123abf6d6dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
