d7be61c230766d59a41e8389e9e5a1ac86c86715
BaekSeungWoo617 <koke100@naver.com>
2024-09-30 10:22:02 +0900

    Add)SoundManager,NAudio,Newtonsoft.Json
    using NAudio.Wave;
    using Newtonsoft.Json;
    SoundManager

---
+++ Text_RPG_24Group/Text_RPG_24Group/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+//https://pixabay.com/ko/sound-effects/ 에서 사운드 파일 가져옴
+namespace Text_RPG_24Group
+{
+    class SoundManager
+    {
+        string SoundBGM { get; set; }
+        string SoundButton {  get; set; }
+        string SoundHurt { get; set; }
+        string SoundHit { get; set; }
+        
+        void PlaySoundBGM(string soundFilePath) 
+        {
+            using (var audioFile = new AudioFileReader(soundFilePath))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+
+                Console.WriteLine("Playing sound...");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();  // 사운드가 끝날 때까지 대기
+            }
+        }
+        void PlaySoundButton()
+        {
+
+        }
+        void PlaySoundHurt()
+        {
+
+        }
+        void PlaySoundHit()
+        {
+
+        }
+    }
+}
--- Text_RPG_24Group/Text_RPG_24Group/obj/Debug/net8.0/Text_RPG_24Group.AssemblyInfo.cs
+++ Text_RPG_24Group/Text_RPG_24Group/obj/Debug/net8.0/Text_RPG_24Group.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46418e99eef81ad63b4219a7f6e1b7a594e41fbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b465c2a7d45b835426c6b411dcc575bdf8ae3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
