316e547151f79ccd5635b19b44f8c44d3d380bb4
BaekSeungWoo617 <koke100@naver.com>
2024-10-02 19:02:26 +0900

    EndingInventory

--- Text_RPG_24Group/Text_RPG_24Group/obj/Debug/net8.0/Text_RPG_24Group.AssemblyInfo.cs
+++ Text_RPG_24Group/Text_RPG_24Group/obj/Debug/net8.0/Text_RPG_24Group.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3452a27752c3dc756baa07d544196237d5f51eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bbbbd7f33ac697c3a915976c906b238fd627f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text_RPG_24Group")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
